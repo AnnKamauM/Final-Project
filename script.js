e59a57597d831ff289dce9b986adca7a06a51b62
Ann Kamau <amuringok@gmail.com>
2025-02-26 15:59:21 +0300

    updated js file prompt for username

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function buyer() {
-  let name = prompt("What is your name?");
+  let firstName = prompt("What is your name?");
   let email = prompt("Please enter your email address");
   let age = prompt("How old are you?");
 
   if (age <= 16) {
     alert(
-      `Sorry ${name}, You need to be 18 to buy our chocolates.We'll be in touch via ${email}💝`
+      `Sorry ${firstName}, You need to be 18 to buy our chocolates.We'll be in touch via ${email}💝`
     );
   } else {
     alert(
-      `Thank you ${name},Enjoy your order.We'll be in touch via ${email}🍫`
+      `Thank you ${firstName},Enjoy your order.We'll be in touch via ${email}🍫`
     );
   }
 }
